0be48b1f3c4faacdb803b2a7e069407eabb02453
gigininder <jshuang1225@gmail.com>
2024-03-06 16:53:04 +0800

    第二次commit (修改文字元件)

--- app/src/main/java/tw/edu/pu/csim/s1114639/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1114639/MainActivity.kt
@@ -2,10 +2,14 @@ package tw.edu.pu.csim.s1114639
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var t: TextView = findViewById(R.id.txv)
+        t.text = "0"
     }
 }
